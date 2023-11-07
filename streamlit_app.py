9df2e9bfd663210ba6b76fd4827e135b8640091c
amstevens99 <150169528+amstevens99@users.noreply.github.com>
2023-11-07 16:34:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header ('Breakfast menu')
-streamlit.text ('Omega 3 & Blueberry Porridge')
-streamlit.text ('Kale, Spinach and Apple Smoothie')
+streamlit.text ('🥣 Omega 3 & Blueberry Porridge')
+streamlit.text ('🥗 Kale, Spinach and Apple Smoothie')
+streamlit.text ('🥑🍞 Avocado Toast')
