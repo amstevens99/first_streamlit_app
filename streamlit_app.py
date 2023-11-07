612023175b9baf12033aaf9fc2f4cef1e1a62e9b
amstevens99 <150169528+amstevens99@users.noreply.github.com>
2023-11-07 16:49:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ streamlit.title('Welcome to the Healthy Diner')
 streamlit.header ('Breakfast Menu')
 streamlit.text ('🥣 Flaxseed & Blueberry Porridge')
 streamlit.text ('🥗 Kale, Spinach and Apple Smoothie')
+streamlit.text ('🐔 Scrambled Eggs')
 streamlit.text ('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Smoothie 🥝🍇')
